d4e65b70bcb64913ba6c75513c7baa058b0c8345
natthakhon <nattlao@gmail.com>
2020-12-01 15:20:51 +0700

    price page

--- src/mock/servicePrice.ts
+++ src/mock/servicePrice.ts
@@ -6,13 +6,23 @@ export class ServicePriceMock{
   getServicePrice():ServicePrice{
     return new ServicePrice("Model 1"
         ,"Category 1"
-        ,[0,0,100,200,300]
+        ,[0,0,100,200,300,400]
+        ,[]
+        ,[]
+        ,[0,0,123,456,789]
         ,[false,false,false,false,false]
-        ,[0,0,123,456,789,1000]
-        ,[false,false,false,false,false]
+        ,[]
         ,false
+        ,1000
+        ,0
         ,false
+        ,2000
+        ,0
         ,false
+        ,0
+        ,0
+        ,0
+        ,0
         )
   }
 }
--- src/vm/servicePrice.ts
+++ src/vm/servicePrice.ts
@@ -1,15 +1,108 @@
-import { DecimalPipe } from '@angular/common';
-
 export class ServicePrice{
   constructor(
     public modelname: string,
     public categoryname: string,
     public warrantyPrice: number[],
     public isWarranty: boolean[],
+    public displayWarranty: number[],
     public onSiteService: number[],
     public isOnSiteService: boolean[],
+    public displayOnsite: number[],
     public isMaintenance: boolean,
+    public maintenancePrice: number,
+    public selectedMaintenancePrice: number,
     public isInstallment: boolean,
-    public isOldMachine: boolean
-  ){  }
+    public installprice: number,
+    public selectedInstallPrice: number,
+    public isOldMachine: boolean,
+    public totalwarranty: number,
+    public totalonsite: number,
+    public total: number,
+    public colmax: number
+  )
+  {
+    this.setvalue();
+  }
+
+  setvalue(){
+    this.setcol็Max();
+    this.setdisplayWarranty();
+    this.setdisplayOnSite();
+    this.setonsite();
+    this.setwarranty();
+    this.total = this.totalwarranty + this.totalonsite +
+                  this.selectedMaintenancePrice + this.selectedInstallPrice;
+  }
+
+  setwarranty(){
+    this.totalwarranty = 0;
+
+    for (let i = 0; i < this.isWarranty.length; i++) {
+      if (this.isWarranty[i] == true){
+        this.totalwarranty = this.warrantyPrice[i];
+        break;
+      }
+    }
+  }
+
+  setonsite(){
+    this.totalonsite = 0;
+
+    for (let i = 0; i < this.isOnSiteService.length; i++) {
+      if (this.isOnSiteService[i] == true){
+        this.totalonsite = this.onSiteService[i];
+        break;
+      }
+    }
+  }
+
+  setMetenancePrice(){
+    if (this.isMaintenance == true){
+      this.selectedMaintenancePrice = this.maintenancePrice;
+    }
+    else{
+      this.selectedMaintenancePrice = 0;
+    }
+  }
+
+  setInstall(){
+    if (this.isInstallment == true){
+      this.selectedInstallPrice = this.installprice;
+    }
+    else{
+      this.selectedInstallPrice = 0;
+    }
+  }
+
+  setcol็Max(){
+    this.colmax=0;
+    if (this.warrantyPrice.length >= this.onSiteService.length){
+      this.colmax = this.warrantyPrice.length
+    }
+    else{
+      this.colmax = this.onSiteService.length
+    }
+  }
+
+  setdisplayWarranty(){
+    for (let i = 0; i < this.colmax; i++) {
+      if (i < this.warrantyPrice.length){
+        this.displayWarranty[i] = this.warrantyPrice[i];
+      }
+      else{
+        this.displayWarranty[i] = 0;
+      }
+    }
+  }
+
+  setdisplayOnSite(){
+    for (let i = 0; i < this.colmax; i++) {
+      if (i < this.onSiteService.length){
+        this.displayOnsite[i] = this.onSiteService[i];
+      }
+      else{
+        this.displayOnsite[i] = 0;
+      }
+    }
+  }
 }
